8d97048bb7512881e1b92568f31d11c428d8c171
Vijay Kasu <vijayalakshmikasu10@gmail.com>
2026-04-14 10:31:51 +0530

    second commit

--- quiz_backend/settings.py
+++ quiz_backend/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
